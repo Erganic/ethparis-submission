bf2af8bad1af0b2394121bd0d88bcb284a8fb62b
Oleksiy Novykov <services@novykov.com>
2023-07-22 01:38:03 +0200

    New distribution management contract

--- src/Ergonomic.sol
+++ src/Ergonomic.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import { EAS } from "eas-contracts/EAS.sol";
+import { Attestation } from "eas-contracts/Common.sol";
+import { IERC20 } from "openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
     @title Ergonomic
@@ -11,18 +13,20 @@ import { EAS } from "eas-contracts/EAS.sol";
 contract Distributor {
 
     EAS public eas;
+    IERC20 public token;
 
     /**
         @notice Mapping of the artists' addresses to their royalties.
      */
-    mapping(address => uint256) public royalties;
+    mapping(address => uint256) public balance;
 
     /**
         @notice Consturctor of this smart contract.
      */
 
-    constructor(address _easAddress) { 
+    constructor(address _easAddress, address _tokenAddress) { 
         eas = new EAS(_easAddress);
+        token = new IERC20(_tokenAddress);
     }
 
     /**
@@ -30,15 +34,32 @@ contract Distributor {
         update the balances of all artisits who have contributed to the branch of the artwork.
         @param _artworkId The uid of attestation record of the purhchased artwork.
      */
-    function buyLicense(uint256 _artworkId) external {
-        
+    function buyLicense(uint256 _artworkId, uint256 amount) external {
+        Attestation memory attestation = eas.getAttestation(_artworkId);
+        uint256 tempAmount;
+
+        if (attestation.refUID == 0) {
+            balance[attestation.attester] += tempAmount;
+            return; // Exit the function after allocating funds to the attester
+        }
+
+        while (attestation.refUID != 0) {
+            balance[attestation.attester] = tempAmount / 50;
+            tempAmount = tempAmount / 50;
+            _artworkId = attestation.refUID;
+            attestation = getAttestation(_artworkId);
+        }
+
     }
 
     /**
-        @notice Internal function that will be used to call the attestation service.
-        @param _artworkId The uid of attestation record of the purhchased artwork.
+        @notice Artist can call this function to withdraw all of their accumulated balance
+        @param _artist The address of the artist
+        @param _amount The amount of the balance to be withdrawn
      */
-    function _callAttestationService(uint256 _artworkId) internal {
-        eas.getAttestation(_artworkId);
-    }
+    function withdraw(address _artist, uint256 _amount) external {
+        require(balance[_artist] >= _amount, "Insufficient balance");
+        balance[_artist] -= _amount;
+        token.transfer(_artist, _amount);
+    }  
 }
